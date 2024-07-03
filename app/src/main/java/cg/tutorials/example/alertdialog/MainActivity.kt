268fda9f811ab4aa522232ff917bcd1d0211d181
Chaitnya-Ghai <chaitnyaghai2004@gmail.com>
2024-07-03 13:28:34 +0530

    binding

--- app/src/main/java/cg/tutorials/example/alertdialog/MainActivity.kt
+++ app/src/main/java/cg/tutorials/example/alertdialog/MainActivity.kt
@@ -8,46 +8,49 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cg.tutorials.example.alertdialog.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
-        var result : EditText?=null
-        var btn1OpenDialog : Button?= null
+//        var result : EditText?=null
+//        var btn1OpenDialog : Button?= null
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        result=findViewById(R.id.etResult)
-        btn1OpenDialog=findViewById(R.id.btn)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        result=findViewById(R.id.etResult)
+//        btn1OpenDialog=findViewById(R.id.btn)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        btn1OpenDialog?.setOnClickListener {
-            var inputText: String = result.text.trim().toString()
+        binding.btn.setOnClickListener {
+            var inputText: String = binding.etResult.text.trim().toString()
             if(inputText.isNotEmpty()){
     AlertDialog.Builder(this).apply {
         setTitle("HELLO!!")
         setMessage(buildString {
-            append("Press + to Add ${result.text}\n ")
-            append("Press - to Add ${result.text?.trim()?.toString()}\n")
+            append("Press + to Add ${binding.etResult.text}\n ")
+            append("Press - to Add ${binding.etResult.text.trim()?.toString()}\n")
             append("Press Reset ")
         })
         setPositiveButton("+") { _, _ ->
-            val currentVal = result.text.toString().toInt()
+            val currentVal = binding.etResult.text.toString().toInt()
             val newVal = currentVal + inputText.toInt()
-            result.setText(newVal.toString())
+            binding.etResult.setText(newVal.toString())
         }
         setNegativeButton("-"){ _ ,_ ->
-            val currentVal = result.text.toString().toInt()
+            val currentVal = binding.etResult.text.toString().toInt()
             val newVal = currentVal - inputText.toInt()
-            result.setText(newVal.toString())
+            binding.etResult.setText(newVal.toString())
         }
         setNeutralButton("Reset"){_,_ ->
             val newVal:String ?= null
-            result.setText(newVal)
+            binding.etResult.setText(newVal)
         }
         setCancelable(false)
         show()
